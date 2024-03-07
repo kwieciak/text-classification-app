e360cde464522305188ea855f7015a35e9db7967
kwieciak <bkwiecinski02@gmail.com>
2024-03-07 18:55:06 +0100

    feat: articles reader works properly

---
+++ project1/src/main/java/org/example/project1/util/Article.java
@@ -0,0 +1,17 @@
+package org.example.project1.util;
+import java.util.List;
+
+
+public class Article {
+    private List<String> placesList;
+    private List<String> topics;
+    private String title;
+    private String text;
+
+    public Article(List<String> placesList, List<String> topics, String title, String text) {
+        this.placesList = placesList;
+        this.topics = topics;
+        this.title = title;
+        this.text = text;
+    }
+}
