ff76603d6e1fa8a62032b76fa3e48cc530d6ec36
mwysocka <mikafikapika@gmail.com>
2024-04-07 20:55:47 +0200

    feat: initial knn

--- project1/src/main/java/org/example/project1/Main.java
+++ project1/src/main/java/org/example/project1/Main.java
@@ -18,10 +18,39 @@ import static java.util.Collections.max;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // Loading all articles
+        String directoryPath = "project1/data";
+        List<Article> articles = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            String filePath = directoryPath + "/reut2-" + String.format("%03d", i) + ".sgm";
+            ArticleReader articleReader = new ArticleReader(filePath);
+            articles.addAll(articleReader.readArticles());
+        }
+        // Extracting features for each article
+        for (Article article : articles) {
+            ArticleFeatures.extractFeatures(article);
+        }
+        ArticleFeatures.normalizeFeatures(articles);
+
+        // kNN classification
+        Metric metric = MetricType.EUCLIDEAN.createMetric();
+        Knn knn = new Knn(3, metric, articles.subList(0, 10), articles.subList(10, 50));
+        List<String> classifiedArticles = knn.classifyArticles();
+        double accuracy = knn.calculateAccuracy(classifiedArticles, articles.subList(10, 50));
+
+        System.out.println("Accuracy: " + accuracy);
+        System.out.println("Classified classes:");
+        for (String classifiedArticle : classifiedArticles) {
+            System.out.println(classifiedArticle);
+        }
+
+
+
+
 //        //Wczytanie wszystkich artykułów
 //        String directoryPath = "project1/data";
 //        List<Article> comparedArticleList = new ArrayList<>();
-//        for (int i = 0; i < 22; i++) {
+//        for (int i = 0; i < 5; i++) {
 //            String filePath = directoryPath + "/reut2-" + String.format("%03d", i) + ".sgm";
 //            ArticleReader articleReader = new ArticleReader(filePath);
 //            comparedArticleList.addAll(articleReader.readArticles());
@@ -41,28 +70,28 @@ public class Main {
 
 
 
-// TO MIKA NAPISAŁA
-        ArticleReader articleReader = new ArticleReader("project1/data/reut2-008.sgm");
-        List<Article> articles = articleReader.readArticles();
-        Metric metric = MetricType.EUCLIDEAN.createMetric();
-        Knn knn = new Knn(3, metric, articles.subList(0,10), articles.subList(10,50));
-        knn.assignFeatureVectors();
-        for (Article article : articles.subList(10,50)) {
-            System.out.println("Feature vector for article " + article.getFeaturesVector());
-        }
-        List<String> classifiedArticles = knn.classifyArticles();
-        double accuracy = knn.calculateAccuracy(classifiedArticles, articles.subList(10,50));
-        System.out.println("Accuracy: " + accuracy);
-        System.out.println(classifiedArticles);
-        System.out.println("Classified classes:");
-        for (String classifiedArticle : classifiedArticles) {
-            System.out.println(classifiedArticle);
-        }
-
-        System.out.println("Actual classes:");
-        for (Article article : articles.subList(10,50)) {
-            System.out.println(article.getPlacesList().get(0));
-        }
+//// TO MIKA NAPISAŁA
+//        ArticleReader articleReader = new ArticleReader("project1/data/reut2-008.sgm");
+//        List<Article> articles = articleReader.readArticles();
+//        Metric metric = MetricType.EUCLIDEAN.createMetric();
+//        Knn knn = new Knn(3, metric, articles.subList(0,10), articles.subList(10,50));
+//        knn.assignFeatureVectors();
+//        for (Article article : articles.subList(10,50)) {
+//            System.out.println("Feature vector for article " + article.getFeaturesVector());
+//        }
+//        List<String> classifiedArticles = knn.classifyArticles();
+//        double accuracy = knn.calculateAccuracy(classifiedArticles, articles.subList(10,50));
+//        System.out.println("Accuracy: " + accuracy);
+//        System.out.println(classifiedArticles);
+//        System.out.println("Classified classes:");
+//        for (String classifiedArticle : classifiedArticles) {
+//            System.out.println(classifiedArticle);
+//        }
+//
+//        System.out.println("Actual classes:");
+//        for (Article article : articles.subList(10,50)) {
+//            System.out.println(article.getPlacesList().get(0));
+//        }
 
 
         //ArticleReader articleReader = new ArticleReader("project1/data/reut2-001.sgm");
