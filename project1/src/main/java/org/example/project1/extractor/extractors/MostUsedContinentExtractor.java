5c3223e11219f24b7206e2b092850b7a77dd0a87
mwysocka <mikafikapika@gmail.com>
2024-04-14 17:51:34 +0200

    refactor: extracting + chart and stats

--- project1/src/main/java/org/example/project1/extractor/extractors/MostUsedContinentExtractor.java
+++ project1/src/main/java/org/example/project1/extractor/extractors/MostUsedContinentExtractor.java
@@ -3,21 +3,16 @@ package org.example.project1.extractor.extractors;
 import org.example.project1.extractor.Extractor;
 import org.example.project1.article.Article;
 import org.example.project1.extractor.WordCounterBuffer;
-import org.example.project1.util.CsvReader;
-
-import java.util.List;
-import java.util.Map;
+import org.example.project1.extractor.WordDictionaries;
 
 public class MostUsedContinentExtractor implements Extractor<String> {
-    private WordCounterBuffer wordCounterBuffer;
-    private List<String> featureTerms;
-    public MostUsedContinentExtractor(WordCounterBuffer wordCounterBuffer, List<String> featureTerms){
+    private final WordCounterBuffer wordCounterBuffer;
+    public MostUsedContinentExtractor(WordCounterBuffer wordCounterBuffer) {
         this.wordCounterBuffer = wordCounterBuffer;
-        this.featureTerms = featureTerms;
     }
     @Override
     public String extract(Article article) {
         String text = wordCounterBuffer.getText(article);
-        return extractStringFeature(text, featureTerms);
+        return extractStringFeature(text, WordDictionaries.getContinentsDictionary());
     }
 }
